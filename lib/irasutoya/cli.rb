da1d98d4dce71846b31926aca186b9381e06047d
Yuji Ueki <unhappychoice@gmail.com>
2019-09-16 09:06:43 +0900

    :tada: Initial commit

---
+++ lib/irasutoya/cli.rb
@@ -0,0 +1,8 @@
+require "irasutoya/cli/version"
+
+module Irasutoya
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/irasutoya/cli/version.rb
@@ -0,0 +1,5 @@
+module Irasutoya
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
