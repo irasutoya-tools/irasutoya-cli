50a0489ac83236945ea8a941372fe2130a6364a6
Yuji Ueki <unhappychoice@gmail.com>
2020-11-06 14:06:00 +0900

    :+1: Implement search command

--- lib/irasutoya/cli.rb
+++ lib/irasutoya/cli.rb
@@ -11,12 +11,27 @@ module Irasutoya
     class Runner < Thor
       desc 'random', 'Gives you random irasutoya image'
       def random
-        irasuto = Irasutoya::Irasuto.random
+        display(Irasutoya::Irasuto.random)
+      end
+
+      desc 'search {query}', 'Gives you 3 irasutoya images by given query'
+      def search(query)
+        Irasutoya::Irasuto
+          .search(query: query)
+          .take(3)
+          .flat_map(&:fetch_irasuto)
+          .compact
+          .each(&method(:display))
+      end
+
+      private
+
+      def display(irasuto)
         say "Page URL:    #{irasuto.url}"
         say "Title:       #{irasuto.title}"
         say "Description: #{irasuto.description}"
-        say "Image URL:   #{irasuto.image_url}"
-        TerminalImage.show_url(irasuto.image_url)
+        irasuto.image_urls.each { |image_url| say "Image URL:   #{image_url}" }
+        irasuto.image_urls.each(&TerminalImage.method(:show_url))
       rescue TerminalImage::UnsupportedTerminal
         say 'warn: This terminal is not able to show images inline', :yellow
         say 'warn: Please use iTerm2 or terminal installed libsixel.', :yellow
